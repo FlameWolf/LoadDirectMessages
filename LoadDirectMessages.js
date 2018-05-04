4f6f9d3e0068bb8309d83b72e41e75ebf8320f89
FlameWolf <agnijith@gmail.com>
2018-05-04 17:49:09 +0530

    Add files via upload

---
+++ LoadDirectMessages.js
@@ -0,0 +1,134 @@
+"use strict";
+var EMPTY_STRING = "";
+var result = EMPTY_STRING;
+var max_entry_id = -1;
+var ajaxResponse = null;
+var conversation_id = "<<CONVERSATION-ID-GOES-HERE>>";
+var baseUrl = `https://twitter.com/messages/with/conversation?id=${conversation_id}`;
+var conversationUrl = baseUrl;
+var itemsAsJSON = null;
+var tweets = [];
+var tweetContainer = null;
+var tweetID = EMPTY_STRING;
+var tweetDirection = EMPTY_STRING;
+var tweetSenderID = EMPTY_STRING;
+var tweetSenderHandle = EMPTY_STRING;
+var tweetSenderName = EMPTY_STRING;
+var tweetTimestamp = EMPTY_STRING;
+var tweetContent = null;
+var attachmentIFrameContainer = null;
+var attachmentUrl = EMPTY_STRING;
+var attachedImage = null;
+var attachedImageUrl = EMPTY_STRING;
+var playableMedia = null;
+var tweetHtml = EMPTY_STRING;
+var quotedTweetContainer = null;
+var quotedTweetID = EMPTY_STRING;
+var quotedTweetSenderID = EMPTY_STRING;
+var quotedTweetSenderHandle = EMPTY_STRING;
+var quotedTweetSenderName = EMPTY_STRING;
+var quotedTweetContent = null;
+var quotedTweet = EMPTY_STRING;
+var terminator = "\n";
+var escapeHtml = function(unsafe) {
+	return unsafe.replace(/&/g, "&#x26;").replace(/</g, "&#x3C;").replace(/>/g, "&#x3E;").replace(/"/g, "&#x22;").replace(/'/g, "&#x27;");
+};
+var currentElement = null;
+var emojoPic = null;
+var hashTagLink = null;
+var timeLineLink = null;
+var cleanUpHtml = function(element) {
+	currentElement = $(element);
+	currentElement.find(".Emoji").each(function(index, value) {
+		emojoPic = $(value);
+		emojoPic.replaceWith(emojoPic.attr("alt"));
+	});
+	currentElement.find(".twitter-hashtag").each(function(index, value) {
+		hashTagLink = $(value);
+		hashTagLink.replaceWith(`<span class=\"hashtag\">${hashTagLink.text()}</span>`);
+	});
+	currentElement.find(".twitter-timeline-link").each(function(index, value) {
+		timeLineLink = $(value);
+		timeLineLink.html(timeLineLink.attr("data-expanded-url"));
+	});
+	return element.innerHTML.replace(/&amp;/g, "&#x26;").replace(/&lt;/g, "&#x3C;").replace(/&gt;/g, "&#x3E;").replace(/&nbsp;/g, "&#xA0;").replace(/\n/g, "&#x0A;");
+};
+var getVideoUrl = function(element) {
+	return $(element).css("background-image").replace(/^url\(\"/, EMPTY_STRING).replace(/\"\)$/, EMPTY_STRING).replace("jpg", "mp4").replace("_preview", EMPTY_STRING).replace("pbs", "video");
+}
+var loadDirectMessages = function() {
+	$.ajax(conversationUrl).done(function(response) {
+		ajaxResponse = JSON.parse(response);
+		if(ajaxResponse.min_entry_id != max_entry_id) {
+			itemsAsJSON = ajaxResponse.items;
+			tweets = Object.keys(itemsAsJSON).map(x => itemsAsJSON[x]).map(function(value, index, array) {
+				tweetContainer = $.parseHTML(value)[0];
+				if(tweetContainer.classList.contains("DirectMessage")) {
+					tweetID = tweetContainer.getAttribute("data-item-id");
+					tweetDirection = (tweetContainer.classList.contains("DirectMessage--sent") ? "sent" : "received");
+					tweetSenderID = tweetContainer.getAttribute("data-sender-id");
+					tweetSenderName = tweetContainer.querySelector(".DMAvatar-image").title;
+					tweetSenderHandle = $(tweetContainer.querySelector(".js-user-profile-link")).attr("href").replace("/", "@");
+					tweetTimestamp = tweetContainer.querySelector("._timestamp").getAttribute("data-time");
+					tweetContent = tweetContainer.querySelector(".DirectMessage-text .tweet-text");
+					attachmentIFrameContainer = tweetContainer.querySelector("[data-card-url]");
+					attachedImage = tweetContainer.querySelector(".dm-attached-media");
+					playableMedia = tweetContainer.querySelector(".PlayableMedia-player");
+					quotedTweetContainer = tweetContainer.querySelector(".QuoteTweet");
+					tweetHtml = (tweetContent == null ? EMPTY_STRING : cleanUpHtml(tweetContent));
+					if(quotedTweetContainer != null) {
+						quotedTweetID = quotedTweetContainer.querySelector(".QuoteTweet-innerContainer").getAttribute("data-item-id");
+						quotedTweetSenderID = quotedTweetContainer.querySelector(".QuoteTweet-innerContainer").getAttribute("data-user-id");
+						quotedTweetSenderName = escapeHtml(quotedTweetContainer.querySelector(".QuoteTweet-fullname").innerText);
+						quotedTweetSenderHandle = quotedTweetContainer.querySelector(".username").innerText;
+						quotedTweetContent = quotedTweetContainer.querySelector(".QuoteTweet-text");
+						quotedTweet = (quotedTweetContent == null ? EMPTY_STRING : cleanUpHtml(quotedTweetContent));
+						if(quotedTweet != EMPTY_STRING)
+							quotedTweet = `<quote id="${quotedTweetID}"><sender id="${quotedTweetSenderID}" handle="${quotedTweetSenderHandle}" name="${quotedTweetSenderName}"/><content>${quotedTweet}</content></quote>`;
+					}
+					else
+						quotedTweet = EMPTY_STRING;
+					if(attachmentIFrameContainer != null) {
+						attachmentUrl = attachmentIFrameContainer.getAttribute("data-card-url");
+						tweetHtml = `<a href="${attachmentUrl}">${attachmentUrl}</a>${tweetHtml != EMPTY_STRING ? "<br/>" : EMPTY_STRING}${tweetHtml}`;
+					}
+					attachedImageUrl = (attachedImage != null ? attachedImage.querySelector("img").src.replace(":large", EMPTY_STRING) : EMPTY_STRING);
+					if(attachedImageUrl != EMPTY_STRING)
+						tweetHtml = `<img class="${attachedImageUrl.match(/^https:\/\/ton\.twimg\.com\/stickers\/stickers\//) ? "sticker" : "image"}" src="${attachedImageUrl}"/>${tweetHtml != EMPTY_STRING ? "<br/>" : EMPTY_STRING}${tweetHtml}`;
+					if(playableMedia != null)
+						tweetHtml = `<video class="video" controls="controls"><source src="${getVideoUrl(playableMedia)}" type="video/mp4"/></video>${tweetHtml != EMPTY_STRING ? "<br/>" : EMPTY_STRING}${tweetHtml}`;
+					return (`<tweet id="${tweetID}" direction="${tweetDirection}" timestamp="${tweetTimestamp}"><sender id="${tweetSenderID}" handle="${tweetSenderHandle}" name="${tweetSenderName}"/>${quotedTweet}<content>${tweetHtml}</content></tweet>`);
+				}
+				else
+					return tweetContainer.outerHTML;
+			});
+			result = `${tweets.join(terminator)}${terminator}${result}`;
+		}
+		if(ajaxResponse.max_entry_id != ajaxResponse.min_entry_id) {
+			max_entry_id = ajaxResponse.min_entry_id;
+			conversationUrl = `${baseUrl}&max_entry_id=${max_entry_id}`;
+			loadDirectMessages();
+		}
+		else {
+			result = `<conversation id="${conversation_id}">${result}</conversation>`;
+			copy(result);
+			console.log("Done! XML data copied to clipboard.");
+			timeLineLink = null;
+			hashTagLink = null;
+			emojoPic = null;
+			currentElement = null;
+			quotedTweetContent = null;
+			quotedTweetContainer = null;
+			tweetContainer = null;
+			playableMedia = null;
+			attachedImage = null;
+			attachmentIFrameContainer = null;
+			tweetContent = null;
+			tweetContainer = null;
+			tweets = null;
+			itemsAsJSON = null;
+			ajaxResponse = null;
+		}
+	});
+};
+loadDirectMessages();
---
+++ Transform-DM-Conversation-XML.xsl
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+	<xsl:output method="html" encoding="UTF-8"/>
+	<xsl:strip-space elements="*"/>
+	<xsl:template match="conversation">
+		<html>
+			<head>
+				<title>DM Conversation</title>
+				<style type="text/css">
+					/*<![CDATA[*/
+					ul.tweets { list-style-type: none; }
+					ul.tweets > li { padding: 5px; width: 50%; clear: both; border-radius: 4px; margin: 5px; }
+					ul.tweets > li.received { float: left; }
+					ul.tweets > li.sent { float: right; }
+					div.tweet-header { padding: 5px; }
+					div.tweet-header, div.quote-header { color: #808080; font-size: smaller; }
+					div.quote-content, div.tweet-content { white-space: pre-line; }
+					div.tweet-content, div.sticker { padding: 5px; border-radius: 4px; }
+					div.tweet-content img.image { max-height: 250px; max-width: 250px; }
+					div.sticker img { max-height: 100px; max-width: 100px; }
+					ul.tweets > li.received div.tweet-content { background-color: #E6ECF0; }
+					ul.tweets > li.sent div.tweet-content { background-color: #1DA1F2; color: #FFFFFF; }
+					a.quote { display: block; border: solid 1px #F0F0F0; border-top-left-radius: 4px; border-top-right-radius: 4px; padding: 5px; text-decoration: none; color: #000000; }
+					a.quote:last-child { border-bottom-left-radius: 4px; border-bottom-right-radius: 4px; }
+					a.quote + div.tweet-content { border-top-left-radius: 0; border-top-right-radius: 0; }
+					ul.tweets > li.sent div.tweet-header { text-align: right; }
+					/*]]>*/
+				</style>
+				<script type="text/javascript">
+					/*<![CDATA[*/
+					var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+					var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+					var formatDate = function(date) {
+						return (days[date.getDay()] + " "
+							+ date.getDate().toString().padStart(2, "0") + " "
+							+ months[date.getMonth()] + " "
+							+ date.getFullYear() + " "
+							+ date.getHours().toString().padStart(2, "0") + ":"
+							+ date.getMinutes().toString().padStart(2, "0") + ":"
+							+ date.getSeconds().toString().padStart(2, "0"));
+					};
+					/*]]>*/
+				</script>
+			</head>
+			<body>
+				<ul class="tweets">
+					<xsl:apply-templates select="tweet"/>
+				</ul>
+				<script type="text/javascript">
+					/*<![CDATA[*/
+					[...document.querySelectorAll("a.quote")].map(function(value, index, array) {
+						var status = value.getAttribute("data-status-id");
+						var handle = value.querySelector("span.sender em.handle").innerText.replace(/^@/, "");
+						value.href = `https://twitter.com/${handle}/status/${status}`;
+					});
+					[...document.querySelectorAll("span.time")].map(x => x.innerHTML = formatDate(new Date(parseInt(x.getAttribute("data-timestamp")) * 1000)));
+					/*]]>*/
+				</script>
+			</body>
+		</html>
+	</xsl:template>
+	<xsl:template match="tweet">
+		<li>
+			<xsl:attribute name="class">
+				<xsl:value-of select="@direction"/>
+			</xsl:attribute>
+			<div class="tweet-header">
+				<xsl:apply-templates select="sender"/>
+				<xsl:text>&#xA0;</xsl:text>
+				<span class="time">
+					<xsl:attribute name="data-timestamp">
+						<xsl:value-of select="@timestamp"/>
+					</xsl:attribute>
+				</span>
+			</div>
+			<xsl:apply-templates select="quote"/>
+			<xsl:if test="content != '' or content/node()">
+				<div class="tweet-content">
+					<xsl:if test="content/img/@class='sticker'">
+						<xsl:attribute name="class">
+							<xsl:text>sticker</xsl:text>
+						</xsl:attribute>
+					</xsl:if>
+					<xsl:copy-of select="content/node()"/>
+				</div>
+			</xsl:if>
+		</li>
+	</xsl:template>
+	<xsl:template match="quote">
+		<a class="quote" target="_blank">
+			<xsl:attribute name="data-status-id">
+				<xsl:value-of select="@id"/>
+			</xsl:attribute>
+			<div class="quote-header">
+				<xsl:apply-templates select="sender"/>
+			</div>
+			<div class="quote-content">
+				<xsl:copy-of select="content/node()"/>
+			</div>
+		</a>
+	</xsl:template>
+	<xsl:template match="sender">
+		<span class="sender">
+			<xsl:attribute name="data-sender-id">
+				<xsl:value-of select="@id"/>
+			</xsl:attribute>
+			<strong class="name">
+				<xsl:value-of select="@name"/>
+			</strong>
+			<xsl:text>&#xA0;</xsl:text>
+			<em class="handle">
+				<xsl:value-of select="@handle"/>
+			</em>
+		</span>
+	</xsl:template>
+</xsl:stylesheet>
